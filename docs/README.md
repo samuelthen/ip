2a9c9c5dc8ae3be762b1516eac48652f26f8fb11
Samuel Then <samuelthen@Samuels-MacBook-Pro.local>
2024-08-22 20:20:13 +0800

    Rename chatbot to Bong and implement greet and exit

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Bong User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Bong.java
@@ -0,0 +1,15 @@
+public class Bong {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________";
+        String msg1 = "Hello! I'm Bong";
+        String msg2 = "What can I do for you?";
+        String msg3 = "Bye. Hope to see you again soon!";
+
+        System.out.println(line);
+        System.out.println(msg1);
+        System.out.println(msg2);
+        System.out.println(line);
+        System.out.println(msg3);
+        System.out.println(line);
+    }
+}
