e6beed20d3bec5a9b7de3b9064d80639e8f4b1ed
Samuel Then <samuelthen@Samuels-MacBook-Pro.local>
2024-08-23 00:33:10 +0800

    Implement ToDos, Events, Deadlines

--- src/main/java/Bong.java
+++ src/main/java/Bong.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
-class Task {
-    private String description;
-    private boolean isDone;
+abstract class Task {
+    protected String description;
+    protected boolean isDone;
 
     public Task(String description) {
         this.description = description;
@@ -19,12 +19,68 @@ class Task {
     }
 
     public String getStatusIcon() {
-        return (isDone ? "[X]" : "[ ]"); // mark done task with X
+        return (isDone ? "[X]" : "[ ]");
+    }
+
+    public abstract String getTypeIcon(); // to be implemented by subclasses
+
+    @Override
+    public abstract String toString(); // to be implemented by subclasses
+}
+
+class Todo extends Task {
+    public Todo(String description) {
+        super(description);
+    }
+
+    @Override
+    public String getTypeIcon() {
+        return "[T]";
     }
 
     @Override
     public String toString() {
-        return getStatusIcon() + " " + description;
+        return getTypeIcon() + getStatusIcon() + " " + description;
+    }
+}
+
+class Deadline extends Task {
+    protected String by;
+
+    public Deadline(String description, String by) {
+        super(description);
+        this.by = by;
+    }
+
+    @Override
+    public String getTypeIcon() {
+        return "[D]";
+    }
+
+    @Override
+    public String toString() {
+        return getTypeIcon() + getStatusIcon() + " " + description + " (by: " + by + ")";
+    }
+}
+
+class Event extends Task {
+    protected String from;
+    protected String to;
+
+    public Event(String description, String from, String to) {
+        super(description);
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public String getTypeIcon() {
+        return "[E]";
+    }
+
+    @Override
+    public String toString() {
+        return getTypeIcon() + getStatusIcon() + " " + description + " (from: " + from + " to: " + to + ")";
     }
 }
 
@@ -84,11 +140,41 @@ public class Bong {
                     System.out.println("     Invalid task number!");
                     System.out.println(line);
                 }
-            } else {
-                Task newTask = new Task(userInput);
+            } else if (userInput.startsWith("todo ")) {
+                String taskDescription = userInput.substring(5).trim();
+                Task newTask = new Todo(taskDescription);
                 list.add(newTask);
                 System.out.println(line);
-                System.out.println("     added: " + userInput);
+                System.out.println("     Got it. I've added this task:");
+                System.out.println("       " + newTask);
+                System.out.println("     Now you have " + list.size() + " tasks in the list.");
+                System.out.println(line);
+            } else if (userInput.startsWith("deadline ")) {
+                String[] parts = userInput.substring(9).split(" /by ");
+                String taskDescription = parts[0].trim();
+                String by = parts[1].trim();
+                Task newTask = new Deadline(taskDescription, by);
+                list.add(newTask);
+                System.out.println(line);
+                System.out.println("     Got it. I've added this task:");
+                System.out.println("       " + newTask);
+                System.out.println("     Now you have " + list.size() + " tasks in the list.");
+                System.out.println(line);
+            } else if (userInput.startsWith("event ")) {
+                String[] parts = userInput.substring(6).split(" /from | /to ");
+                String taskDescription = parts[0].trim();
+                String from = parts[1].trim();
+                String to = parts[2].trim();
+                Task newTask = new Event(taskDescription, from, to);
+                list.add(newTask);
+                System.out.println(line);
+                System.out.println("     Got it. I've added this task:");
+                System.out.println("       " + newTask);
+                System.out.println("     Now you have " + list.size() + " tasks in the list.");
+                System.out.println(line);
+            } else {
+                System.out.println(line);
+                System.out.println("     I'm sorry, I don't understand that command.");
                 System.out.println(line);
             }
         }
