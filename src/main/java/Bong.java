1ced91501ee09b0d2b8c610e2128bbe55f7d9bdc
Samuel Then <samuelthen@Samuels-MacBook-Pro.local>
2024-08-22 21:06:20 +0800

    Implement echo function

--- src/main/java/Bong.java
+++ src/main/java/Bong.java
@@ -1,15 +1,34 @@
+import java.util.Scanner;
+
 public class Bong {
     public static void main(String[] args) {
-        String line = "____________________________________________________________";
-        String msg1 = "Hello! I'm Bong";
-        String msg2 = "What can I do for you?";
-        String msg3 = "Bye. Hope to see you again soon!";
+        String line = "    ____________________________________________________________";
+        String openingMsg1 = "    Hello! I'm Bong";
+        String openingMsg2 = "    What can I do for you?";
+        String endingMsg = "    Bye. Hope to see you again soon!";
 
         System.out.println(line);
-        System.out.println(msg1);
-        System.out.println(msg2);
+        System.out.println(openingMsg1);
+        System.out.println(openingMsg2);
         System.out.println(line);
-        System.out.println(msg3);
-        System.out.println(line);
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String userInput = scanner.nextLine();
+
+            if (userInput.equals("bye")) {
+                System.out.println(line);
+                System.out.println(endingMsg);
+                System.out.println(line);
+                break;
+            }
+
+            System.out.println(line);
+            System.out.println("    " + userInput);
+            System.out.println(line);
+        }
+
+        scanner.close();
     }
 }
